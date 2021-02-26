dc7e50442c7353be27e40cb5c024c7ae6d5fc0fb
jonmferreira <jonmferreira@gmail.com>
2021-02-26 13:14:48 -0400

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="teste-roga">
+    <v-app-bar
+        app
+    >
+      <div style="
+        padding-left: 1000px;"
+      >John Doe</div>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+              v-for="n in 3"
+              :key="n"
+              cols="4"
+              >
+          <v-card
+              height="200"
+              width="400"
+              elevation="6">
+              <v-avatar
+
+                color="teal"
+                size="60"
+                rounded
+              > HI </v-avatar>
+          </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  //
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
